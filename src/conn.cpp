cf31b263ea3b0d6c27dc8d434be356dbebbbe9e3
Bepartofyou <309554135@qq.com>
2017-12-12 16:28:33 +0800

    check kcp interval 0

--- src/conn.cpp
+++ src/conn.cpp
@@ -47,7 +47,7 @@ int Conn::init_kcp(kcpuv_conv_t conv) {
 	_kcp->output = on_kcp_output;
 
 	//r = ikcp_nodelay(_kcp, 1, 10, 2, 1);
-	r = ikcp_nodelay(_kcp, 1, 1, 2, 1);
+	r = ikcp_nodelay(_kcp, 1, 0, 2, 1);
 	PROC_ERR(r);
 	// wnd
 	r = ikcp_wndsize(_kcp, 8192, 8192);
