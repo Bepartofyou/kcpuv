2523306146bf7bddcda2e7e1d8c3501dcf4196f4
Bepartofyou <309554135@qq.com>
2017-12-12 17:47:14 +0800

    upd

--- src/conn.cpp
+++ src/conn.cpp
@@ -48,7 +48,7 @@ int Conn::init_kcp(kcpuv_conv_t conv) {
 
 	//r = ikcp_nodelay(_kcp, 1, 10, 2, 1);
 	//r = ikcp_nodelay(_kcp, 1, 1, 2, 1);
-	r = ikcp_nodelay(_kcp, 0, 1, 0, 0);
+	r = ikcp_nodelay(_kcp, 0, 1, 0, 1);
 	PROC_ERR(r);
 	// wnd
 	r = ikcp_wndsize(_kcp, 65535, 65535);
