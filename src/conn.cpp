24479b915b413510e3f0ab4fbd81f0f6932d6bdb
Bepartofyou <309554135@qq.com>
2017-12-12 17:44:39 +0800

    use delay nofast control

--- src/conn.cpp
+++ src/conn.cpp
@@ -47,7 +47,8 @@ int Conn::init_kcp(kcpuv_conv_t conv) {
 	_kcp->output = on_kcp_output;
 
 	//r = ikcp_nodelay(_kcp, 1, 10, 2, 1);
-	r = ikcp_nodelay(_kcp, 1, 1, 2, 1);
+	//r = ikcp_nodelay(_kcp, 1, 1, 2, 1);
+	r = ikcp_nodelay(_kcp, 0, 1, 0, 0);
 	PROC_ERR(r);
 	// wnd
 	r = ikcp_wndsize(_kcp, 65535, 65535);
